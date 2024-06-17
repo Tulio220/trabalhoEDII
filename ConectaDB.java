94cfb735b554a1547a87ad970755407d4466fd52
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-17 20:49:50 -0300

    Update ConectaDB.java

--- ConectaDB.java
+++ ConectaDB.java
@@ -1,24 +1,29 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConectaDB {
+    //conexao com BD
     //atributo
     private Connection conexao;
+
     //construtor
+    //conecta no Banco de Dados
     public ConectaDB(){
         String url = "jdbc:mariadb://localhost:3306/ed2";
         String user = "root";
-        String pwd = "root";
-        
+        String pwd = "98245803";
+
         try {
-            conexao = DriverManager.getConnection(url,user,pwd);
-            System.out.println("Conexão estabelecida!");
-        } catch (Exception e){
-            System.out.println("Falha na conexão!");
+            conexao = DriverManager.getConnection(url, user, pwd);
+            System.out.println("Conexão Realizada");
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            //TOdo: handle exception
+            System.out.println("Nao encontrado drive");
         }
     }
+
+    //metodo para retornar a conexao com o BD
     public Connection getConexaoDB(){
         return conexao;
     }
