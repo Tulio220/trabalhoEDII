89cca7a2754993c9447f30714fe28733c3d9e8fe
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-17 20:50:16 -0300

    Update LivroDAO.java

--- LivroDAO.java
+++ LivroDAO.java
@@ -1,34 +1,19 @@
+
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.LinkedList;
 
 public class LivroDAO {
     public void inserir(Livro livro){
         ConectaDB conexao = new ConectaDB();
-        String sql = "INSERT INTO livro (titulo, autor, ano) values(?,?,?)";
+        String sql = "INSERT INTO livro(titulo, autor, ano) values(?, ?, ?)";
         try {
             PreparedStatement pst = conexao.getConexaoDB().prepareStatement(sql);
             pst.setString(1, livro.getTitulo());
             pst.setString(2, livro.getAutor());
-            pst.setInt(3, livro.getAno_publicacao());
+            pst.setInt(3, livro.getAnoPublicacao());
             pst.execute();
-            System.out.println("Inserção ok:" + livro);
+            System.out.println("Insercao ok: "+ livro);
         } catch (Exception e) {
-            //TODO: handle exception
-            System.out.println("Falha na Inserção:" + e.getMessage());
-        }
-    }
-
-    public LinkedList<Livro> consultarTodos(){
-        ConectaDB conexao = new ConectaDB();
-        String sql = "SELECT * FROM livro";
-        LinkedList<Livro> lista = new LinkedList<Livro>();
-        try{
-            prepareStatement pst = conexao.getConexaoDB().prepareStatement(sql);
-            //executar consulta
-            ResultSet resultados = pst.executeQuery();
-        }catch(Exception e){
-            System.out.println("Falha na consulta livro: " + e.getMessage());
+            System.out.println("Falha na inserção: "+ e.getMessage());
         }
     }
 }
