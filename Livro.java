70c7e21eb9c03093b6e4b4daefaab25d0a0d2434
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-17 21:06:35 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,57 +1,52 @@
 public class Livro {
-    static String getContador;
     private int id;
     private String titulo;
     private String autor;
     private int anoPublicacao;
-    private static int contador = 0;
-    
-    public Livro(String titulo){
-        this.setTitulo(titulo);
-        contador ++;
-        this.setId(contador);
-    }
-    public void setId(int contador){
-        this.id = contador;
-    }
 
-    private void setTitulo(String titulo){
-        titulo = titulo.toUpperCase();
+    public Livro(int id, String titulo, String autor, int anoPublicacao) {
+        this.id = id;
         this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
     }
 
-    public int getContador(){
-        return  this.contador;
-    }
-    
-
-    public String getTitulo(){
-        return this.titulo;
+    public int getId() {
+        return id;
     }
 
-    public int getId(){
-        return this.id;
+    public String getTitulo() {
+        return titulo;
     }
 
     public String getAutor() {
         return autor;
     }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
     public int getAnoPublicacao() {
         return anoPublicacao;
     }
 
-    public void setAnoPublicacao(int anoPublicacao) {
-        this.anoPublicacao = anoPublicacao;
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
     }
 
-    //criar toString 
-    public String toString(){
-        return "ID: " + getId() + " Titulo: " + getTitulo() + " Autor: " + getAutor() + " Ano de publicação: " + getAnoPublicacao();
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Livro livro = (Livro) obj;
+        return id == livro.id;
     }
-    
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
 }
