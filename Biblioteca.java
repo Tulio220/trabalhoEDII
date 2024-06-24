5b7f097e0d092eb4fb55f91c8feb6f9a14691cb3
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-24 19:34:02 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -10,60 +10,68 @@ public class Biblioteca {
     }
 
     // Métodos para gerenciar livros
-    public void adicionarLivro(Livro livro) {
+    public void inserir(Livro livro) {
         livros.add(livro);
         livros.sort((Livro l1, Livro l2) -> l1.getId() - l2.getId());
     }
 
-    public Livro buscarLivro(int id) {
+    public Livro consultaID(int id) {
         for (Livro livro : livros) {
             if (livro.getId() == id) {
+                System.out.println(livro);
                 return livro;
             }
         }
+        System.out.println("Livro não encontrado.");
         return null;
     }
 
-    public boolean removerLivro(int id) {
-        Livro livro = buscarLivro(id);
+    public boolean remover(int id) {
+        Livro livro = consultaID(id);
         if (livro != null) {
             livros.remove(livro);
+            System.out.println("Livro removido com sucesso.");
             return true;
         }
+        System.out.println("Falha ao remover o livro.");
         return false;
     }
 
-    public void listarLivros() {
+    public void listarTodos() {
         for (Livro livro : livros) {
             System.out.println(livro);
         }
     }
 
     // Métodos para gerenciar usuários
-    public void adicionarUsuario(Usuario usuario) {
+    public void inserirUsuario(Usuario usuario) {
         usuarios.add(usuario);
         usuarios.sort((Usuario u1, Usuario u2) -> u1.getId() - u2.getId());
     }
 
-    public Usuario buscarUsuario(int id) {
+    public Usuario consultaIDUsuario(int id) {
         for (Usuario usuario : usuarios) {
             if (usuario.getId() == id) {
+                System.out.println(usuario);
                 return usuario;
             }
         }
+        System.out.println("Usuário não encontrado.");
         return null;
     }
 
     public boolean removerUsuario(int id) {
-        Usuario usuario = buscarUsuario(id);
+        Usuario usuario = consultaIDUsuario(id);
         if (usuario != null) {
             usuarios.remove(usuario);
+            System.out.println("Usuário removido com sucesso.");
             return true;
         }
+        System.out.println("Falha ao remover o usuário.");
         return false;
     }
 
-    public void listarUsuarios() {
+    public void listarTodosUsuarios() {
         for (Usuario usuario : usuarios) {
             System.out.println(usuario);
         }
