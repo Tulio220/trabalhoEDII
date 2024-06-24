289518382b9d245c64c1e7f3b46a80827b216a7c
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-24 19:34:22 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -2,11 +2,11 @@ public class Usuario {
     private int id;
     private String nome;
     private String email;
+    private static int contador = 0;
 
-    public Usuario(int id, String nome, String email) {
-        this.id = id;
+    public Usuario(String nome) {
+        this.id = ++contador;
         this.nome = nome;
-        this.email = email;
     }
 
     public int getId() {
@@ -17,12 +17,37 @@ public class Usuario {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
-        return "Usuario [ID=" + id + ", Nome=" + nome + ", Email=" + email + "]";
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Usuario usuario = (Usuario) obj;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
     }
 }
