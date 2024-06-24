b20c9003485cbc16032ef974baf377f1c8da3e27
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-24 19:33:32 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -3,12 +3,11 @@ public class Livro {
     private String titulo;
     private String autor;
     private int anoPublicacao;
+    private static int contador = 0;
 
-    public Livro(int id, String titulo, String autor, int anoPublicacao) {
-        this.id = id;
+    public Livro(String titulo) {
+        this.id = ++contador;
         this.titulo = titulo;
-        this.autor = autor;
-        this.anoPublicacao = anoPublicacao;
     }
 
     public int getId() {
@@ -19,16 +18,46 @@ public class Livro {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
     }
 
     public int getAnoPublicacao() {
         return anoPublicacao;
     }
 
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
     @Override
     public String toString() {
-        return "Livro [ID=" + id + ", Título=" + titulo + ", Autor=" + autor + ", Ano de Publicação=" + anoPublicacao + "]";
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Livro livro = (Livro) obj;
+        return id == livro.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
     }
 }
