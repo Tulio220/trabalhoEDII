ec5c2523d4d989a37e74114ac0e5e7f7b0b677b2
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-24 19:19:21 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -29,24 +29,6 @@ public class Livro {
 
     @Override
     public String toString() {
-        return "Livro{" +
-                "id=" + id +
-                ", titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", anoPublicacao=" + anoPublicacao +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Livro livro = (Livro) obj;
-        return id == livro.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Integer.hashCode(id);
+        return "Livro [ID=" + id + ", Título=" + titulo + ", Autor=" + autor + ", Ano de Publicação=" + anoPublicacao + "]";
     }
 }
