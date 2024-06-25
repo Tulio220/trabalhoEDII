4c77fe6e64577ccacdf0de3a36c08cf326088df0
Tulio Barros <125105595+Tulio220@users.noreply.github.com>
2024-06-24 21:21:09 -0300

    Update Maindb.java

--- Maindb.java
+++ Maindb.java
@@ -1,36 +1,130 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class MainDB {
     public static void main(String[] args) {
-        ConectaDB conectaDB = new ConectaDB();
-        Biblioteca biblioteca = new Biblioteca(conectaDB.getConexaoDB());
+        Scanner scanner = new Scanner(System.in);
+        Biblioteca bib = new Biblioteca();
+        Biblioteca user = new Biblioteca();
+        LivroDAO objDAO = new LivroDAO();
+        UsuarioDao userDao = new UsuarioDao();
+         
+        while (true) { 
+            System.out.println("-----MENU-----");
+            System.out.println("1- Cadastro de livros:");
+            System.out.println("2- Cadastro de usuários:");
+            System.out.println("0- Sair:");
+            System.out.println("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+            
+            if(opcao == 1){
+                System.out.println("-----MENU-----");
+                System.out.println("1- Inserir Livro:");
+                System.out.println("2- Busca livro por ID:");
+                System.out.println("3- Remover livro por ID:");
+                System.out.println("4- Listar livros:");
+                System.out.println("0- Menu anterior:");
+                int opcao2 = scanner.nextInt();
+                scanner.nextLine();
 
-        // Inserção de Livros
-        biblioteca.inserir(new Livro(1, "Livro A", "Autor A", 2000));
-        biblioteca.inserir(new Livro(2, "Livro B", "Autor B", 2010));
-        biblioteca.inserir(new Livro(3, "Livro C", "Autor C", 2020));
+                if (opcao2 == 1){
+                    System.out.println("Digite o título do livro:");
+                    String titulo = scanner.nextLine();
+                    System.out.println("Digite o autor do livro:");
+                    String autor = scanner.nextLine();
+                    System.out.println("Digite o ano de publicação do livro:");
+                    int ano = scanner.nextInt();
 
-        // Busca de Livro por ID
-        System.out.println("Busca de Livro ID 2: " + biblioteca.consultaID(2));
+                    Livro livro = new Livro(titulo);
+                    livro.setAutor(autor);
+                    livro.setAnoPublicacao(ano);
 
-        // Remoção de Livro por ID
-        biblioteca.remover(2);
+                    objDAO.inserir(livro);
 
-        // Listagem de Livros
-        System.out.println("Listagem de Livros: ");
-        biblioteca.listarTodos();
+                }else if (opcao2 == 2){
+                    System.out.println("Digite o ID do livro que deseja buscar:");
+                    int id = scanner.nextInt();
+                    
+                    System.out.println(objDAO.consultar(id));
+                    
 
-        // Inserção de Usuários
-        biblioteca.inserirUsuario(new Usuario(1, "Usuario A", "emailA@exemplo.com"));
-        biblioteca.inserirUsuario(new Usuario(2, "Usuario B", "emailB@exemplo.com"));
-        biblioteca.inserirUsuario(new Usuario(3, "Usuario C", "emailC@exemplo.com"));
+                }else if (opcao2 == 3){
+                    System.out.println("Digite o ID do livro que deseja remover:");
+                    int id = scanner.nextInt();
 
-        // Busca de Usuário por ID
-        System.out.println("Busca de Usuario ID 2: " + biblioteca.consultaIDUsuario(2));
+                    objDAO.excluir(id);
 
-        // Remoção de Usuário por ID
-        biblioteca.removerUsuario(2);
+                }else if (opcao2 == 4){
+                    LinkedList<Livro> dados = objDAO.consultarTodos();
+                    int j = dados.size();
+                    for (Livro valor : dados) {
+                        System.out.println(valor);
+                        
+                    }
+                    
+                }else if (opcao2 == 0){
+                    System.out.println("Retornando...");
+                
+                }else{
+                    System.out.println("Opcao inválida. Tente novamente.");
+                }
+            }else if (opcao == 2){
+                System.out.println("-----MENU-----");
+                System.out.println("1- Inserir usuário:");
+                System.out.println("2- Buscar usuário por ID:");
+                System.out.println("3- Remover usuário por ID:");
+                System.out.println("4- Listar usuários:");
+                System.out.println("0- Menu anterior:");
+                int opcao2 = scanner.nextInt();
+                scanner.nextLine();
 
-        // Listagem de Usuários
-        System.out.println("Listagem de Usuários: ");
-        biblioteca.listarTodosUsuarios();
+                if (opcao2 == 1){
+                    System.out.println("Digite o nome do usuário:");
+                    String nome = scanner.nextLine();
+                    System.out.println("Digite o e-mail do usuário:");
+                    String email = scanner.nextLine();
+
+                    Usuario usuario = new Usuario(nome);
+                    usuario.setEmail(email);
+
+                    userDao.inserirUsuario(usuario);
+
+                }else if (opcao2 == 2){
+                    System.out.println("Digite o ID do usuario que deseja buscar:");
+                    int id = scanner.nextInt();
+                    
+                    System.out.println(userDao.consultarUsuario(id));
+                    
+
+                }else if (opcao2 == 3){
+                    System.out.println("Digite o ID do usuário que deseja remover:");
+                    int id = scanner.nextInt();
+
+                    userDao.excluirUsuario(id);
+
+                }else if (opcao2 == 4){
+                    LinkedList<Usuario> dados = userDao.consultarTodosUsuarios();
+                    int j = dados.size();
+                    for (Usuario valor : dados) {
+                        System.out.println(valor);
+                        
+                    }
+                    
+                }else if (opcao2 == 0){
+                    System.out.println("Retornando...");
+                
+                }else{
+                    System.out.println("Opcao inválida. Tente novamente.");
+                }
+
+            }else if  (opcao== 0){
+                System.out.println("Saindo...");
+                scanner.close();
+                return;
+            }else{
+                System.out.println("Opçcão inválida, tente novamente.");
+                }
+        }
     }
 }
